dc6ef7d36301c9da1fd2642a645e3332d5cc2d10
Jeremy Bernard <jeremy.bernard@iex.ec>
2026-04-16 17:29:22 +0200

    feat: rework and add new parameters to NATS configuration

--- src/config.rs
+++ src/config.rs
@@ -9,16 +9,23 @@ pub struct ServerConfig {
 }
 
 #[derive(Deserialize)]
+pub struct NatsConfig {
+    pub url: String,
+    pub stream_name: String,
+    pub consumer_name: String,
+    pub consumer_max_deliver: i64,
+    pub max_ack_pending: i64,
+    pub max_batch: i64,
+}
+
+#[derive(Deserialize)]
 pub struct Config {
     pub server: ServerConfig,
     pub chain_id: u64,
     pub rpc_url: String,
     pub nox_compute_contract_address: Address,
+    pub nats: NatsConfig,
     pub handle_gateway_url: String,
-    pub nats_url: String,
-    pub nats_stream_name: String,
-    pub nats_consumer_name: String,
-    pub nats_consumer_max_deliver: i64,
     pub wallet_key: String,
 }
 
@@ -33,10 +40,12 @@ impl Config {
                 "0x0000000000000000000000000000000000000000",
             )?
             .set_default("handle_gateway_url", "http://localhost:3000")?
-            .set_default("nats_url", "nats://localhost:4222")?
-            .set_default("nats_stream_name", "nox_ingestor")?
-            .set_default("nats_consumer_name", "nox_ingestor_consumer")?
-            .set_default("nats_consumer_max_deliver", 3)?
+            .set_default("nats.url", "nats://localhost:4222")?
+            .set_default("nats.stream_name", "nox_ingestor")?
+            .set_default("nats.consumer_name", "nox_ingestor_consumer")?
+            .set_default("nats.consumer_max_deliver", 10)?
+            .set_default("nats.max_ack_pending", 10)?
+            .set_default("nats.max_batch", 10)?
             .add_source(
                 Environment::with_prefix("NOX_RUNNER")
                     .prefix_separator("_")
